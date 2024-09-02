49516ff80c66b6602bf478f68db6c1fbe11171c0
Wendelsena <wendel4019@gmail.com>
2024-09-02 16:43:44 -0300

    StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,17 @@ main() {
   runApp(new PerguntaApp()); // Chama a função runApp e passa o widget 'PerguntaApp' como argumento
 }
 
-// Cria uma classe 'PerguntaApp' que é um StatelessWidget
-class PerguntaApp extends StatelessWidget {
+// Cria uma classe 'PerguntaAppState' que gerencia o estado do widget 'PerguntaApp'
+class PerguntaAppState extends State<PerguntaApp> {
+  var perguntaSelecionada = 0; // Variável que armazena o índice da pergunta atual
+
+  void responder() {
+    setState(() {
+      perguntaSelecionada++; // Incrementa o valor da variável 'perguntaSelecionada' ao chamar 'responder'
+    });
+    print(perguntaSelecionada); // Imprime o valor atualizado de 'perguntaSelecionada' no console
+  }
+
   @override
   Widget build(BuildContext context) { // O método build é obrigatório em um StatelessWidget, e é onde a interface do widget é definida
     final List<String> perguntas = [  // Declara uma lista de strings chamada 'perguntas'
@@ -21,20 +30,20 @@ class PerguntaApp extends StatelessWidget {
         ),
         body: Column( // Column é um widget que organiza seus filhos em uma coluna vertical
           children: <Widget>[
-            Text(perguntas[0]),  // Exibe o primeiro item da lista 'perguntas'
+            Text(perguntas[perguntaSelecionada]),  // Exibe o primeiro item da lista 'perguntas'
 
             // Cria três botões elevados, cada um com um texto diferente
             ElevatedButton(
               child: Text('Resposta 1'),
-              onPressed: null, 
+              onPressed: responder, // passa a função 'responder' como parâmetro
             ),
             ElevatedButton(
               child: Text('Resposta 2'),
-              onPressed: null, 
+              onPressed: responder, 
             ),
             ElevatedButton(
               child: Text('Resposta 3'),
-              onPressed: null, 
+              onPressed: responder, 
             ),
           ],
         ),
@@ -42,3 +51,13 @@ class PerguntaApp extends StatelessWidget {
     );
   }
 }
+
+// Cria uma classe 'PerguntaApp' que é um StatefulWidget
+class PerguntaApp extends StatefulWidget {
+
+  @override
+  PerguntaAppState createState() {
+    return PerguntaAppState(); // Retorna uma nova instância da classe de estado associada a 'PerguntaApp'
+  }
+}
+
